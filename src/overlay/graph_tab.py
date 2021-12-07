c85bc6c9cbd086b9a094f71acfdf0db6994e6005
FluffyMaguro <uminomaguro@gmail.com>
2021-12-07 04:01:25 +0100

    ...

--- src/overlay/graph_tab.py
+++ src/overlay/graph_tab.py
@@ -3,53 +3,49 @@ from typing import Any, Dict, List, Optional
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+from overlay.graph_widget import GraphWidget
 from overlay.logging_func import get_logger
-from overlay.plotting import Figure
 from overlay.settings import settings
 from overlay.worker import scheldule
 
 logger = get_logger(__name__)
 
 
+
 class GraphTab(QtWidgets.QWidget):
     def __init__(self, parent):
         super().__init__(parent)
+
+        # self.setAutoFillBackground(True)
+        layout = QtWidgets.QVBoxLayout()
+        self.setLayout(layout)
+
         # Graph
-        self.graph = QtWidgets.QLabel(self)
+        self.test_graph = GraphWidget()
+        layout.addWidget(self.test_graph)
 
-        # Refresh button
-        refresh = QtWidgets.QPushButton("Rescale", self)
-        refresh.setStatusTip("Rescales the graph based on new size")
-        refresh.clicked.connect(self.plot_data)
-        refresh.move(QtCore.QPoint(10, 10))
-
-        # ** get data somewhere
-
-        # ** PLOT Ratings, Ranks
+        # self.test_graph2 = GraphWidget()
+        # layout.addWidget(self.test_graph2)
 
         # Get data
         scheldule(self.get_data_finished, self.get_rating_debug_3v3)
+
 
     def get_data_finished(self, data: List[Any]):
         self.data = data
         self.plot_data()
 
     def plot_data(self):
-        # Update size
-        new_width = self.parent().parent().width()
-        new_height = self.parent().parent().height() - 20
-        self.graph.setGeometry(0, 0, new_width, new_height)
-
         # Demo of plotting
         y = [i['rating'] for i in self.data]
         x = list(range(len(y)))
 
-        fig = Figure("3v3 Rating history", new_width, new_height)
-        fig.x_label = "Games"
-        fig.y_label = "Rating"
-        fig.plot(x, y, label="3v3 rating")
-        fig.text("Some text", 2, 1100)
-        self.graph.setPixmap(fig.get_pixmap())
+        self.test_graph.title="3v3 Rating history"
+        self.test_graph.x_label = "Games"
+        self.test_graph.y_label = "Rating"
+        self.test_graph.plot(x, y, label="3v3 rating")
+        self.test_graph.plot(x, [i+100 for i in y], label="3v3 rating2")
+        self.test_graph.text("Some text", 2, 1100)
 
     def get_rating_debug_3v3(self):
         time.sleep(0.1)
